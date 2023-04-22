15d1059889bbb687683803ba94becfc38bd23470
S1lv3rn <34184510+S1lv3rn@users.noreply.github.com>
2023-04-22 15:05:53 +0100

    made canvas resizable in testing/flexCanvas, wip

---
+++ testing/flexCanvas/script.js
@@ -0,0 +1,13 @@
+// Get the canvas element and its context
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+
+
+window.onload = function () {
+  
+    ctx.font = "30px Arial";
+    ctx.fillText("Generate Pattern!", 30, 50);
+};
+
+
